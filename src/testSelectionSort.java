cd50dd63d536c4ba19630f3c054939bb0fa6a99e
Abidul Chowdury <chowdury@umich.edu>
2019-12-08 23:39:42 -0500

    changed something

--- src/testSelectionSort.java
+++ src/testSelectionSort.java
@@ -14,7 +14,7 @@ class testSelectionSort {
 
     public testSelectionSort() {
     }
-    
+/*
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -22,30 +22,30 @@ class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
+
+//        /** add tests to check for this unit test *
 
         System.out.println("Positive data: ");
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(arr[i]);
-        }   
-        
+        }
+
         System.out.println("Sorted Positive data: ");
-        
+
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(Sortedarr[i]);
-        }        
+        }
     }
-    
-    
-    
+
+
+
     public void testNegative(){
         int[] arr = new int[5];
         arr[0] = -8;
@@ -53,27 +53,27 @@ class testSelectionSort {
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = -2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -2;
         Sortedarr[1] = -7;
         Sortedarr[2] = -8;
         Sortedarr[3] = -9;
         Sortedarr[4] = -10;
-        /** Test data contains negative values only **/
-        
+//        /** Test data contains negative values only
+
         System.out.println("Negative data: ");
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(arr[i]);
-        }   
-        
+        }
+
         System.out.println("Sorted Negative data: ");
-        
+
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(Sortedarr[i]);
-        }       
+        }
     }
- /*   
+ */
     public void testMixed(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -81,56 +81,56 @@ class testSelectionSort {
         arr[2] = 7;
         arr[3] = 0;
         arr[4] = -2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
-        Sortedarr[4] = 0;       
-        /** Test data contains with both positive, negative and zeros **/
-        
+        Sortedarr[4] = 0;
+//        /** Test data contains with both positive, negative and zeros
+
         System.out.println("mixed data: ");
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(arr[i]);
-        }   
-        
+        }
+
         System.out.println("Sorted mixed data: ");
-        
+
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(Sortedarr[i]);
-        }        
+        }
     }
-    
+
     public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
+
+//        /** Test data contains duplicates
         int[] arr = new int[5];
         arr[0] = 8;
         arr[1] = 8;
         arr[2] = 7;
         arr[3] = 0;
         arr[4] = -2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 8;
-        Sortedarr[4] = 0;       
-        /** Test data contains with both positive, negative and zeros **/
-        
+        Sortedarr[4] = 0;
+//        /** Test data contains with both positive, negative and zeros *
+
         System.out.println("duplicate data: ");
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(arr[i]);
-        }   
-        
+        }
+
         System.out.println("Sorted duplicate data: ");
-        
+
         for (int i = 0; i < arr.length; ++i) {
         	System.out.println(Sortedarr[i]);
-        }     	
+        }
     }
 
-*/
+
 }
